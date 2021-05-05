284a6837d0cc12960e1f0c5d98ef94a293385344
John Doe <shamailov@gmail.com>
2021-05-05 23:20:24 +0300

    Login form - only design

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Alert, Container, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './LoginPage.css'
 
 function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [showInvalidLogin, setShowInvalidLogin] = useState(false);
+
+
+
     return (
+
         <Container className="p-login">
-            <h1 className="display-1">כניסת לקוחות רשומים</h1>
+            <div className="loginContainer">
+                <div class="col-sm-4 signup-vertical-sep"></div>
+                <div class="col-sm-4 signup-vertical-sep-text">כניסה ללקוחות</div>
+                <div class="col-sm-4 signup-vertical-sep"></div>
+                {showInvalidLogin ? <Alert variant="danger">שגיאה בהזנת הנתונים</Alert> : null}
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>דוא"ל</Form.Label>
+                        <Form.Control type="email" className="dir-ltr-left"
+                            value={email} onChange={e => setEmail(e.target.value)} />
+                        <Form.Text className="text-muted">
+                        אנו לעולם לא נשתף אותו עם אף גורם אחר
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>סיסמה</Form.Label>
+                        <Form.Control type="password" className="dir-ltr-left"
+                            value={pwd} onChange={e => setPwd(e.target.value)} />
+                    </Form.Group>
+                    <Button variant="success" type="submit" block>כניסה</Button>
+                </Form>
+
+                <Link to="/signup">עוד לא רשום?</Link>
+            </div>
         </Container>
+
     );
 }
 
